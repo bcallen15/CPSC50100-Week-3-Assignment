62ed1bd575cc5c8e30ba38fe28a021817e3943b7
bcallen15 <67833609+bcallen15@users.noreply.github.com>
2020-07-09 07:24:08 -0500

    Lab1Ex2

---
+++ Week 1 Assigment/src/GradeAvg.java
@@ -0,0 +1,15 @@
+
+public class GradeAvg {
+
+	public static void main(String[] args) {
+		double quizScore1 = 85, quizScore2 = 74, quizScore3 = 94; 
+		double testScore1 = 66, testScore2 = 87;
+		double quizScoreAvg = ((quizScore1 + quizScore2 + quizScore3)/3);
+		double testScoreAvg = ((testScore1 + testScore2)/2);
+		System.out.println(quizScoreAvg + "%");
+		System.out.println(testScoreAvg + "%");
+		
+
+	}
+
+}
