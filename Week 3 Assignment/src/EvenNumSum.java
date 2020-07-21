6bdac72051b1d29fe818bb7377bea49b29507f8c
bcallen15 <67833609+bcallen15@users.noreply.github.com>
2020-07-20 21:21:32 -0500

    first half of example2

--- Week 3 Assignment/src/EvenNumSum.java
+++ Week 3 Assignment/src/EvenNumSum.java
@@ -5,7 +5,17 @@ import java.util.Scanner;
 public class EvenNumSum {
 
 	public static void main(String[] args) {
-		Scanner scan = new Scanner
+		int num;
+		
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Enter a number: ");
+		num = scan.nextInt();
+		
+		if (num<2) {
+			System.out.println("Error, number is less than 2.");
+			System.out.println("Please enter a number that is at least 2.");
+			num = scan.nextInt();
+		}
 
 	}
 
