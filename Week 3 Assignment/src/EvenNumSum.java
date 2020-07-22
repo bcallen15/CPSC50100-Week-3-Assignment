ddc5651a161ded15fdef3e2d8626cea7031f13f9
bcallen15 <67833609+bcallen15@users.noreply.github.com>
2020-07-21 19:02:03 -0500

    Lab3Ex2 final

--- Week 3 Assignment/src/EvenNumSum.java
+++ Week 3 Assignment/src/EvenNumSum.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class EvenNumSum {
 
 	public static void main(String[] args) {
-		int num;
+		int num, sum, x;
 		
 		Scanner scan = new Scanner(System.in);
 		System.out.println("Enter a number: ");
@@ -16,6 +16,13 @@ public class EvenNumSum {
 			System.out.println("Please enter a number that is at least 2.");
 			num = scan.nextInt();
 		}
+		
+		if(num%2!=0) {  //checks remainder to see if number is even (even divided by two has no remainder)
+			num--;
+		}
+		x = num/2;
+		sum = x * (x+1);
+		System.out.println("The sum of all even numbers from 2 to your number is: " + sum);
 
 	}
 
